bf47ee0e0487912ddfcbe8460c4a89528c0c7f36
covfefezz <1224129913@qq.com>
2020-05-11 03:07:27 +0800

    '完成homefeature'

---
+++ src/components/common/tabcontrol/TabControl.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="tab-control">
+    <div v-for="(item,index) in titles" class="tab-control-item" :class="{active:currentIndex===index}" @click="itemClick(index)">
+      <span>{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "TabControl",
+        props:{
+            titles:Array
+        },
+        data(){
+            return{
+                currentIndex:0
+            }
+        },
+        methods:{
+            itemClick(index){
+                this.currentIndex = index
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+  .tab-control{
+    display: flex;
+    height: 40px;
+    line-height: 40px;
+    text-align: center;
+    font-size: 15px;
+  }
+  .tab-control-item{
+    flex:1
+  }
+  .tab-control-item span{
+    padding:5px;
+  }
+  .active{
+    color:#FC4366
+  }
+
+  .active span{
+    border-bottom: 2px solid #FC4366;
+  }
+</style>
---
+++ src/views/Home/childComps/HomeFeature.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="home-feature">
+    <img  src="@/assets/img/home/recommend_bg.jpg" alt="">
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "HomeFeature"
+    }
+</script>
+
+<style scoped>
+  #home-feature{
+
+  }
+  #home-feature img{
+    width: 100%;
+  }
+</style>
