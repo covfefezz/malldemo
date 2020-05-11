e0ff1553090c8f31f38e497f024135ff9b7b6947
covfefezz <1224129913@qq.com>
2020-05-11 18:01:43 +0800

    完成商品展示

---
+++ src/components/common/goods/Goods.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="goods">
+    <!--<div><button @click="btnclick">anjian</button></div>-->
+    <goods-item v-for="item in goods" :item="item"></goods-item>
+  </div>
+</template>
+
+<script>
+
+  import GoodsItem from "./GoodsItem"
+  export default {
+    name: "Goods",
+
+    components:{GoodsItem},
+
+    props:{
+      goods:Array
+    },
+
+    methods:{
+      btnclick(){
+        console.log(this.goods)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+  .goods{
+    width:100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+  }
+</style>
